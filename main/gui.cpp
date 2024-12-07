b0dc65bc18b05a63d6d95a976ac7e7169f104d41
Ben Mens <ben.mens@gmail.com>
2024-12-07 13:57:28 +0100

    first working version

--- main/gui.cpp
+++ main/gui.cpp
@@ -1,48 +1,27 @@
 #include "gui.hpp"
 
 #include <stdio.h>
-#include <sx127x.h>
 
 #include "driver/i2c.h"
-#include "esp-display.hpp"
+#include "esp-display-backlight.hpp"
+#include "esp-display-ili9341.hpp"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "lvgl-display.hpp"
-#include "rotary_encoder.hpp"
 #include "sdkconfig.h"
-
-extern sx127x *sx127xDevice;
 
 #define TAG "gui"
 
-#if CONFIG_PRJ_ST7798_SPI1_HOST == 1
-#define ST9978_SPI SPI1_HOST
-#elif CONFIG_PRJ_ST7798_SPI2_HOST == 1
-#define ST9978_SPI SPI2_HOST
-#elif CONFIG_PRJ_ST7798_SPI3_HOST == 1
-#define ST9978_SPI SPI3_HOST
-#endif
+#define ILI9341_SPI SPI2_HOST
 
 ESP_EVENT_DEFINE_BASE(GUI_EVENTS);
 
-static RotaryEncoder *encoder;
-static esp_lcd_panel_io_handle_t ssd1306_panel_io_handle;
-static esp_lcd_panel_handle_t ssd1306_panel_handle;
-static esp_lcd_panel_io_handle_t st7798_panel_io_handle;
-static esp_lcd_panel_handle_t st7798_panel_handle;
+static esp_lcd_panel_io_handle_t ili9341_panel_io_handle;
+static esp_lcd_panel_handle_t ili9341_panel_handle;
 
 static lv_group_t *g_group;
-static lv_obj_t *screen1, *screen2;
+static lv_obj_t *screen1;
 static lv_obj_t *labelIpAddress;
-static lv_obj_t *slider_label;
-static lv_obj_t *slider;
-static lv_obj_t *cb_power;
-static int8_t lowPowerOptions[] = {-4, -3, -2, -1, 0,  1,  2,  3,  4, 5,
-                                   6,  7,  8,  9,  10, 11, 12, 13, 14};
-static int8_t highPowerOptions[] = {2,  3,  4,  5,  6,  7,  8,  9, 10,
-                                    11, 12, 13, 14, 15, 16, 17, 20};
-static bool highPower = false;
-static uint8_t powerIndex = 0;
 
 extern uint8_t test_img[];
 
@@ -55,7 +34,7 @@ void setIpAddressText(char *text)
     lcd::lvglUnlock();
 }
 
-lv_obj_t *infoScreen()
+lv_obj_t *createScreen1()
 {
     lv_obj_t *label;
 
@@ -66,6 +45,7 @@ lv_obj_t *infoScreen()
     label = lv_label_create(scr);
     lv_label_set_text(label, "IP Adress:");
     lv_obj_set_pos(label, 0, 0);
+    lv_obj_set_style_text_color(label, LV_COLOR_MAKE(0xE5, 0x00, 0x00), NULL);
 
     labelIpAddress = lv_label_create(scr);
     lv_label_set_text(labelIpAddress, "-");
@@ -76,182 +56,31 @@ lv_obj_t *infoScreen()
     return scr;
 }
 
-static void update_power_settings()
-{
-    lv_state_t cbState = lv_obj_get_state(cb_power);
-
-    if (!(cbState & LV_STATE_CHECKED)) {
-        lv_slider_set_range(slider, 0, sizeof(lowPowerOptions) - 1);
-        if (highPower) {
-            highPower = false;
-            powerIndex = 0;
-            lv_slider_set_value(slider, powerIndex, LV_ANIM_OFF);
-        }
-    } else {
-        lv_slider_set_range(slider, 0, sizeof(highPowerOptions) - 1);
-        if (!highPower) {
-            highPower = true;
-            powerIndex = 0;
-            lv_slider_set_value(slider, powerIndex, LV_ANIM_OFF);
-        }
-    }
-
-    powerIndex = lv_slider_get_value(slider);
-
-    int16_t power;
-    if (!highPower) {
-        power = lowPowerOptions[powerIndex];
-        ESP_ERROR_CHECK(
-            sx127x_tx_set_pa_config(SX127x_PA_PIN_RFO, power, sx127xDevice));
-    } else {
-        power = highPowerOptions[powerIndex];
-        ESP_ERROR_CHECK(
-            sx127x_tx_set_pa_config(SX127x_PA_PIN_BOOST, power, sx127xDevice));
-    }
-
-    char buf[8];
-    snprintf(buf, sizeof(buf), "%d", (int16_t)power);
-    lv_label_set_text(slider_label, buf);
-}
-
-static void event_handler(lv_event_t *e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *target = lv_event_get_target(e);
-
-    if (target == slider && code == LV_EVENT_VALUE_CHANGED) {
-        update_power_settings();
-    } else if (target == cb_power && code == LV_EVENT_VALUE_CHANGED) {
-        update_power_settings();
-    }
-}
-
-lv_obj_t *createLoraControllScreen()
-{
-    lcd::lvglLock(-1);
-
-    lv_obj_t *scr = lv_obj_create(NULL);
-    lv_obj_set_style_pad_all(scr, 20, LV_STATE_DEFAULT);
-
-    // cb_power = lv_checkbox_create(scr);
-    cb_power = lv_switch_create(scr);
-    // lv_checkbox_set_text(cb_power, "High power");
-    // lv_obj_set_style_text_font(cb_power, &lv_font_montserrat_14, 0);
-    // lv_obj_set_size(cb_power, 30, 20);
-    lv_obj_add_event_cb(cb_power, event_handler, LV_EVENT_ALL, NULL);
-    lv_group_add_obj(g_group, cb_power);
-
-    lv_obj_t *label = lv_label_create(scr);
-    lv_obj_align_to(label, cb_power, LV_ALIGN_OUT_RIGHT_TOP, 10, 0);
-    lv_checkbox_set_text(label, "High power");
-
-    slider = lv_slider_create(scr);
-    lv_obj_set_size(slider, LV_PCT(100), 20);
-    lv_obj_align_to(slider, cb_power, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 20);
-    lv_obj_add_event_cb(slider, event_handler, LV_EVENT_ALL, NULL);
-    lv_group_add_obj(g_group, slider);
-    lv_obj_set_style_radius(slider, 0, 0);
-    lv_obj_set_style_radius(slider, 0, LV_PART_INDICATOR);
-
-    slider_label = lv_label_create(scr);
-    lv_label_set_text(slider_label, "-");
-    lv_obj_set_style_text_align(slider_label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_size(slider_label, LV_PCT(100), LV_SIZE_CONTENT);
-    lv_obj_align_to(slider_label, slider, LV_ALIGN_TOP_LEFT, 0, 0);
-
-    update_power_settings();
-
-    lcd::lvglUnlock();
-
-    return scr;
-}
-
-// Menlo, Monaco, 'Courier New', monospace
-
-lv_obj_t *createTestScreen1()
-{
-    lcd::lvglLock(-1);
-
-    lv_obj_t *scr = lv_obj_create(NULL);
-    lv_obj_set_style_pad_all(scr, 20, LV_STATE_DEFAULT);
-
-    lv_obj_t *sw = lv_switch_create(scr);
-    lv_group_add_obj(g_group, sw);
-    lv_obj_set_pos(sw, 0, 0);
-
-    lcd::lvglUnlock();
-
-    return scr;
-}
-
-static void custom_apply_cb(struct _lv_theme_t *theme, lv_obj_t *obj)
-{
-    static bool initialized = false;
-    static lv_style_t style;
-
-    if (!initialized) {
-        initialized = true;
-        lv_style_init(&style);
-        // lv_style_set_line_color(&style, lv_palette_main(LV_PALETTE_RED));
-        // lv_style_set_border_color(&style, lv_palette_main(LV_PALETTE_RED));
-        lv_style_set_outline_color(&style, lv_palette_main(LV_PALETTE_RED));
-        // lv_style_set_bg_color(&style, lv_palette_main(LV_PALETTE_RED));
-        lv_style_set_outline_width(&style, 5);
-    }
-
-    if (lv_obj_check_type(obj, &lv_checkbox_class)) {
-        lv_obj_add_style(obj, &style, LV_PART_INDICATOR | LV_STATE_EDITED);
-    } else if (lv_obj_check_type(obj, &lv_slider_class)) {
-        lv_obj_add_style(obj, &style, LV_PART_KNOB | LV_STATE_EDITED);
-    }
-}
-
 esp_err_t guiInit()
 {
     lcd::startLvgl();
 
-    ssd1306_panel_io_handle = display::createSsd1306I2CPanelIO(
-        (esp_lcd_i2c_bus_handle_t)I2C_NUM_0, 0x3c);
+    ili9341_panel_io_handle = display::createIli9341SpiPanelIO(
+        ILI9341_SPI, (gpio_num_t)CONFIG_PRJ_PIN_ILI9341_CS,
+        (gpio_num_t)CONFIG_PRJ_PIN_ILI9341_DC, 8, 8, 40 * 1000 * 1000);
 
-    ssd1306_panel_handle = display::createSsd1306Panel(ssd1306_panel_io_handle);
-
-    esp_lcd_panel_mirror(ssd1306_panel_handle, true, true);
+    ili9341_panel_handle = display::createIli9341Panel(
+        ili9341_panel_io_handle, (gpio_num_t)CONFIG_PRJ_PIN_ILI9341_RES);
 
     lv_disp_t *disp1 =
-        lcd::registerDisplay(ssd1306_panel_io_handle, ssd1306_panel_handle,
-                             lcd::SSD1306, 128, 64, 24);
-
-    lv_theme_t *th1 = lv_theme_mono_init(disp1, false, &lv_font_montserrat_12);
-
-    static lv_theme_t th_new1;
-    lv_theme_set_parent(&th_new1, th1);
-
-    lv_disp_set_theme(disp1, &th_new1);
-    th_new1.font_large = &lv_font_montserrat_20;
-
-    st7798_panel_io_handle = display::createSt7789SpiPanelIO(
-        ST9978_SPI, (gpio_num_t)CONFIG_PRJ_PIN_ST7789_CS,
-        (gpio_num_t)CONFIG_PRJ_PIN_ST7789_DC, 8, 8, 20 * 1000 * 1000);
-
-    st7798_panel_handle = display::createSt7789Panel(
-        st7798_panel_io_handle, (gpio_num_t)CONFIG_PRJ_PIN_ST7789_RES);
-
-    lv_disp_t *disp2 = lcd::registerDisplay(
-        st7798_panel_io_handle, st7798_panel_handle, lcd::ST7789, 240, 240, 16);
-
-    display::setupBacklightPin((gpio_num_t)CONFIG_PRJ_PIN_ST7789_BK_LIGHT);
-    display::setBacklight((gpio_num_t)CONFIG_PRJ_PIN_ST7789_BK_LIGHT, 1);
+        lcd::registerDisplay(ili9341_panel_io_handle, ili9341_panel_handle,
+                             lcd::ILI9341, 320, 240, 16);
 
     lv_theme_t *th2 = lv_theme_default_init(
-        NULL, lv_palette_main(LV_PALETTE_BLUE),
-        lv_palette_main(LV_PALETTE_CYAN), true, &lv_font_montserrat_14);
+        NULL, lv_palette_main(LV_PALETTE_RED),
+        lv_palette_main(LV_PALETTE_YELLOW), false, &lv_font_montserrat_12);
 
     static lv_theme_t th_new2;
     lv_theme_set_parent(&th_new2, th2);
 
-    lv_theme_set_apply_cb(&th_new2, custom_apply_cb);
-
-    lv_disp_set_theme(disp2, &th_new2);
+    lv_disp_set_theme(disp1, &th_new2);
+    th_new2.font_small = &lv_font_montserrat_20;
+    th_new2.font_normal = &lv_font_montserrat_20;
     th_new2.font_large = &lv_font_montserrat_20;
 
     g_group = lv_group_create();
@@ -268,22 +97,14 @@ esp_err_t guiInit()
         }
     });
 
-    encoder = new RotaryEncoder((gpio_num_t)CONFIG_PRJ_PIN_ROTARY_CLK,
-                                (gpio_num_t)CONFIG_PRJ_PIN_ROTARY_DATA,
-                                (gpio_num_t)CONFIG_PRJ_PIN_ROTARY_SWITCH);
-    lcd::createLvglRotaryInputDev(*encoder);
-
-    lv_indev_set_group(lcd::rotary_indev, g_group);
-
     lv_disp_set_default(disp1);
-    screen1 = infoScreen();
+
+    screen1 = createScreen1();
+
     lv_scr_load(screen1);
 
-    lv_disp_set_default(disp2);
-
-    screen2 = createLoraControllScreen();
-    lv_scr_load(screen2);
-    createTestScreen1();
+    display::setupBacklightPin((gpio_num_t)CONFIG_PRJ_PIN_ILI9341_BK_LIGHT);
+    display::setBacklight((gpio_num_t)CONFIG_PRJ_PIN_ILI9341_BK_LIGHT, 1);
 
     return ESP_OK;
 }
--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,10 @@
 #include <string.h>
-#include <sx127x.h>
 
-#include "df-player.hpp"
+#include "driver/gpio.h"
 #include "driver/i2c.h"
 #include "driver/spi_master.h"
 #include "driver/uart.h"
-#include "esp-display.hpp"
+#include "esp-display-backlight.hpp"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
 #include "esp_adc/adc_oneshot.h"
@@ -22,223 +21,7 @@
 
 #define NUM_LEDS 1
 
-#define MP3_RX 15
-#define MP3_TX 7
-
 static Pixels *neoPixels = NULL;
-
-#if CONFIG_PRJ_LORA_SPI1_HOST == 1
-#define LORA_SPI SPI1_HOST
-#elif CONFIG_PRJ_LORA_SPI2_HOST == 1
-#define LORA_SPI SPI2_HOST
-#elif CONFIG_PRJ_LORA_SPI3_HOST == 1
-#define LORA_SPI SPI3_HOST
-#endif
-
-struct LoraMsg {
-    int id;
-    int counter;
-};
-
-static adc_oneshot_unit_handle_t oneshot_handle;
-
-sx127x *sx127xDevice = NULL;
-TaskHandle_t handle_interrupt;
-int messages_sent = 0;
-int supported_power_levels[] = {
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20,
-};
-int supported_power_levels_count = sizeof(supported_power_levels) / sizeof(int);
-int current_power_level = 0;
-
-void IRAM_ATTR handle_interrupt_fromisr(void *arg)
-{
-    xTaskResumeFromISR(handle_interrupt);
-}
-
-void handle_interrupt_task(void *arg)
-{
-    while (1) {
-        vTaskSuspend(NULL);
-        sx127x_handle_interrupt((sx127x *)arg);
-    }
-}
-
-void tx_callback(sx127x *device)
-{
-    if (messages_sent > 0) {
-        ESP_LOGI(TAG, "transmitted");
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
-
-    LoraMsg msg = {
-        .id = 42,
-        .counter = messages_sent,
-    };
-
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission((uint8_t *)&msg,
-                                                        sizeof(msg), device));
-
-    ESP_ERROR_CHECK(
-        sx127x_set_opmod(SX127x_MODE_TX, SX127x_MODULATION_LORA, device));
-    ESP_LOGI(TAG, "transmitting message %u", messages_sent);
-
-    messages_sent++;
-}
-
-void cad_callback(sx127x *device, int cad_detected)
-{
-    if (cad_detected == 0) {
-        ESP_LOGI(TAG, "cad not detected");
-        ESP_ERROR_CHECK(
-            sx127x_set_opmod(SX127x_MODE_CAD, SX127x_MODULATION_LORA, device));
-        return;
-    }
-    // put into RX mode first to handle interrupt as soon as possible
-    ESP_ERROR_CHECK(
-        sx127x_set_opmod(SX127x_MODE_RX_CONT, SX127x_MODULATION_LORA, device));
-    ESP_LOGI(TAG, "cad detected\n");
-}
-
-void setupLora()
-{
-    spi_device_interface_config_t dev_cfg;
-    memset(&dev_cfg, 0, sizeof(dev_cfg));
-    dev_cfg.command_bits = 0;
-    dev_cfg.address_bits = 8;
-    dev_cfg.dummy_bits = 0;
-    dev_cfg.mode = 0;
-    dev_cfg.clock_source = SPI_CLK_SRC_DEFAULT;
-    dev_cfg.clock_speed_hz = 1000000;
-    dev_cfg.spics_io_num = (gpio_num_t)(CONFIG_PRJ_LORA_PIN_SS);
-    dev_cfg.queue_size = 16;
-
-    spi_device_handle_t lora_spi_device;
-    ESP_ERROR_CHECK(spi_bus_add_device(LORA_SPI, &dev_cfg, &lora_spi_device));
-
-    ESP_ERROR_CHECK(sx127x_create(lora_spi_device, &sx127xDevice));
-
-    // uint8_t registers[0x80];
-    // sx127x_dump_registers(registers, sx127xDevice);
-
-    // for (unsigned int i = 0; i < 0x80; i++) {
-    //     ESP_LOGI(TAG, "%02x = %02x", i, registers[i]);
-    // }
-
-    ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_SLEEP, SX127x_MODULATION_LORA,
-                                     sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_set_frequency(865123456, sx127xDevice));
-
-    ESP_ERROR_CHECK(sx127x_lora_reset_fifo(sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_STANDBY,
-                                     SX127x_MODULATION_LORA, sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_lora_set_bandwidth(SX127x_BW_125000, sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_lora_set_implicit_header(NULL, sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_lora_set_modem_config_2(SX127x_SF_9, sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_lora_set_syncword(18, sx127xDevice));
-    ESP_ERROR_CHECK(sx127x_set_preamble_length(8, sx127xDevice));
-    sx127x_tx_set_callback(tx_callback, sx127xDevice);
-
-    BaseType_t task_code = xTaskCreatePinnedToCore(
-        handle_interrupt_task, "handle interrupt", 8196, sx127xDevice, 2,
-        &handle_interrupt, xPortGetCoreID());
-    if (task_code != pdPASS) {
-        ESP_LOGE(TAG, "can't create task %d", task_code);
-        sx127x_destroy(sx127xDevice);
-        return;
-    }
-
-    gpio_set_direction((gpio_num_t)CONFIG_PRJ_LORA_PIN_DIO0, GPIO_MODE_INPUT);
-    gpio_pulldown_en((gpio_num_t)CONFIG_PRJ_LORA_PIN_DIO0);
-    gpio_pullup_dis((gpio_num_t)CONFIG_PRJ_LORA_PIN_DIO0);
-    gpio_set_intr_type((gpio_num_t)CONFIG_PRJ_LORA_PIN_DIO0, GPIO_INTR_POSEDGE);
-    gpio_isr_handler_add((gpio_num_t)CONFIG_PRJ_LORA_PIN_DIO0,
-                         handle_interrupt_fromisr, (void *)sx127xDevice);
-
-    ESP_ERROR_CHECK(
-        sx127x_tx_set_pa_config(SX127x_PA_PIN_BOOST, 13, sx127xDevice));
-    sx127x_tx_header_t header = {.enable_crc = true,
-                                 .coding_rate = SX127x_CR_4_5};
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_explicit_header(&header, sx127xDevice));
-
-    tx_callback(sx127xDevice);
-}
-
-static int scanI2CBus()
-{
-    uint8_t address;
-    printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
-    for (int i = 0; i < 128; i += 16) {
-        printf("%02x: ", i);
-        for (int j = 0; j < 16; j++) {
-            fflush(stdout);
-            address = i + j;
-
-            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, 0x01);
-            i2c_master_stop(cmd);
-
-            esp_err_t ret =
-                i2c_master_cmd_begin(I2C_NUM_0, cmd, 50 / portTICK_PERIOD_MS);
-
-            i2c_cmd_link_delete(cmd);
-
-            if (ret == ESP_OK) {
-                printf("%02x ", address);
-            } else if (ret == ESP_ERR_TIMEOUT) {
-                printf("UU ");
-            } else {
-                printf("-- ");
-            }
-        }
-        printf("\r\n");
-    }
-
-    return 0;
-}
-
-void setupJoystick()
-{
-    adc_oneshot_unit_init_cfg_t init_config2 = {
-        .unit_id = ADC_UNIT_2,
-        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
-        .ulp_mode = ADC_ULP_MODE_DISABLE,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &oneshot_handle));
-
-    adc_oneshot_chan_cfg_t acd_config = {
-        .atten = ADC_ATTEN_DB_12,
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-    };
-
-    ESP_ERROR_CHECK(
-        adc_oneshot_config_channel(oneshot_handle, ADC_CHANNEL_4, &acd_config));
-
-    ESP_ERROR_CHECK(
-        adc_oneshot_config_channel(oneshot_handle, ADC_CHANNEL_5, &acd_config));
-
-    adc_cali_handle_t calibration_handle = NULL;
-    esp_err_t ret = ESP_FAIL;
-
-    ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
-    adc_cali_curve_fitting_config_t cali_config = {
-        .unit_id = ADC_UNIT_2,
-        .chan = ADC_CHANNEL_4,
-        .atten = ADC_ATTEN_DB_12,
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-    };
-    ret =
-        adc_cali_create_scheme_curve_fitting(&cali_config, &calibration_handle);
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "Calibration Success");
-
-    } else if (ret == ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else {
-        ESP_LOGE(TAG, "Invalid arg or no memory");
-    }
-}
 
 extern "C" void app_main(void)
 {
@@ -252,7 +35,7 @@ extern "C" void app_main(void)
     neoPixels->setPixel(0, {
                                .red = 0x00,
                                .green = 0x00,
-                               .blue = 0x08,
+                               .blue = 0x00,
                                .white = 0x00,
                            });
     neoPixels->write();
@@ -269,111 +52,15 @@ extern "C" void app_main(void)
         .data6_io_num = 0,
         .data7_io_num = 0,
         .max_transfer_sz =
-            24 * 240 * 2 + 8,  // TODO parameterize this based on LCD settings
+            320 * 80 *
+            sizeof(uint16_t),  // TODO parameterize this based on LCD settings
         .flags = 0,
         .isr_cpu_id = (esp_intr_cpu_affinity_t)0,
         .intr_flags = 0,
     };
     ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg2, SPI_DMA_CH_AUTO));
 
-    ESP_LOGI(TAG, "setup SPI3_HOST");
-    spi_bus_config_t buscfg3 = {
-        .mosi_io_num = (gpio_num_t)(CONFIG_PRJ_PIN_SPI3_MOSI),
-        .miso_io_num = (gpio_num_t)(CONFIG_PRJ_PIN_SPI3_MISO),
-        .sclk_io_num = (gpio_num_t)(CONFIG_PRJ_PIN_SPI3_SCK),
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .data4_io_num = 0,
-        .data5_io_num = 0,
-        .data6_io_num = 0,
-        .data7_io_num = 0,
-        .max_transfer_sz = 200000,
-        .flags = 0,
-        .isr_cpu_id = (esp_intr_cpu_affinity_t)0,
-        .intr_flags = 0,
-    };
-    ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &buscfg3, SPI_DMA_CH_AUTO));
-
-    setupLora();
-
-    ESP_LOGI(TAG, "Initialize I2C bus");
-    i2c_config_t i2c_conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = (gpio_num_t)(CONFIG_PRJ_PIN_SDA),
-        .scl_io_num = (gpio_num_t)(CONFIG_PRJ_PIN_SCL),
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master =
-            {
-                .clk_speed = CONFIG_PRJ_I2C_CLOCK_SPEED,
-            },
-        .clk_flags = 0,
-    };
-    ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &i2c_conf));
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
-
-    scanI2CBus();
-
     guiInit();
-
-    // uart_config_t uart_config = {
-    //     .baud_rate = 9600,
-    //     .data_bits = UART_DATA_8_BITS,
-    //     .parity = UART_PARITY_DISABLE,
-    //     .stop_bits = UART_STOP_BITS_1,
-    //     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    //     .rx_flow_ctrl_thresh = 0,
-    //     .source_clk = UART_SCLK_DEFAULT,
-    //     .flags =
-    //         {
-    //             .backup_before_sleep = 0,
-    //         },
-    // };
-
-    // Configure UART parameters
-    // const int uart_buffer_size = (200);
-    // QueueHandle_t uart_queue;
-    // Install UART driver using an event queue here
-    // ESP_ERROR_CHECK(
-    //     uart_driver_install(UART_NUM_2, uart_buffer_size, 0, 0, NULL, 0));
-
-    // ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &uart_config));
-
-    // ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, MP3_TX, MP3_RX, UART_PIN_NO_CHANGE,
-    //                              UART_PIN_NO_CHANGE));
-
-    // setupJoystick();
-
-    // static int adc_raw[2];
-    // static int voltage[2];
-
-    // DFPlayerMini_Fast player;
-
-    // player.begin(UART_NUM_2, false, 1000);
-
-    // player.volume(10);
-
-    // while (true) {
-    //     player.playFromMP3Folder(15);
-    //     player.query(0x42, 0, 0);
-
-    //     // ESP_ERROR_CHECK(
-    //     //     adc_oneshot_read(oneshot_handle, ADC_CHANNEL_4, &adc_raw[0]));
-    //     // ESP_ERROR_CHECK(
-    //     //     adc_oneshot_read(oneshot_handle, ADC_CHANNEL_5, &adc_raw[1]));
-    //     // ESP_LOGI(TAG, "x=%d y=%d", adc_raw[0], adc_raw[1]);
-    //     // if (calibration_handle) {
-    //     //     // ESP_ERROR_CHECK(adc_cali_raw_to_voltage(calibration_handle,
-    //     //     //                                         adc_raw[0],
-    //     //     //                                         &voltage[0]));
-    //     //     // ESP_ERROR_CHECK(adc_cali_raw_to_voltage(calibration_handle,
-    //     //     //                                         adc_raw[1],
-    //     //     //                                         &voltage[1]));
-    //     //     ESP_LOGI(TAG, "x=%d mV y=%d mV", voltage[0], voltage[1]);
-    //     // }
-
-    //     vTaskDelay(10000 / portTICK_PERIOD_MS);
-    // }
 
     vTaskDelay(2000 / portTICK_PERIOD_MS);
 }
